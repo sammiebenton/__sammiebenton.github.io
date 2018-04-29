ebe9869c70f6e10c66de3824bcf290c5b9941986
Unknown <info@sammiebenton.com>
2018-04-29 19:53:50 -0400

    nav set up

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row-fluid" id="app">
+  	<Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar';
+export default {
+  name: 'app',
+  components: {
+		Navbar
+  }
+}
+</script>
+
+<style lang="scss">
+	@import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+	@import './assets/scss/mastyle.css';
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a class="navbar-brand" href="#">{{brand}}</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse d-lg-flex justify-content-xl-center code2" id="mainNav">
+			<ul class="navbar-nav">
+				<li class="nav-item active" v-for="nav in navs">
+					<a class="nav-link" :href="nav.href">{{nav.link}}</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+</template>
+
+
+<script>
+	import 'bootstrap';
+	export default {
+		name: "Navbar",
+		data() {
+			return {
+				brand: "",
+				navs : [
+					{
+						link: "Home",
+						href: "#"
+					},
+					{
+						link: "About",
+						href: "#"
+					},
+					{
+						link: "Models & Options",
+						href: "#"
+					},
+					{
+						link: "News",
+						href: "#"
+					},
+					{
+						link: "Gallery",
+						href: "#"
+					},
+					{
+						link: "Contact",
+						href: "#"
+					}
+				]
+			}
+		}
+	}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
