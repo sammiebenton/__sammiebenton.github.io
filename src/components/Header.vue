f3ffb7e35a8985f5d506236c527c57930be6826c
Unknown <info@sammiebenton.com>
2018-05-01 21:23:42 -0400

    new home page

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row" id="header">
+    	<div class="col-6" id="takeItIn"></div>
+    </div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	#header{
+		height: 750px;
+		background: url('../assets/img/jstips.gif') center top;
+		background-size: contain;
+	}
+</style>
